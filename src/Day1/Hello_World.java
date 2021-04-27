a628ddec6c562cdc4a9c558b07eadffc1ec065e6
Stanislav Pryimak <st.pryimak@gmail.com>
2021-04-27 13:15:16 -0500

    Added one more line of code

--- src/Day1/Hello_World.java
+++ src/Day1/Hello_World.java
@@ -3,5 +3,7 @@ package Day1;
 public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
     }
 }
